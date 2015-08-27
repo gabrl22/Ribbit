68eef42a850d22215b150447290e33ec06770f04
Gabriel Cole <lgcw92@gmail.com>
2015-08-27 02:12:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/gabriel/ribbit/RibbitApplication.java
@@ -0,0 +1,23 @@
+package com.example.gabriel.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by Gabriel on 8/26/15.
+ */
+public class RibbitApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "xASTd4yuL6FSRkmprkGYUg95D6Xgmpim2kvDsyWA"
+                , "X4sekSCvA9CKWgjkr1pNO83qxtMGtN9e9WFbZPS5");
+
+
+    }
+}
---
+++ app/src/main/java/com/example/gabriel/ribbit/SignUpActivity.java
@@ -0,0 +1,105 @@
+package com.example.gabriel.ribbit;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Bind(R.id.user_name_field)EditText mUserName;
+    @Bind(R.id.password_field)EditText mPassword;
+    @Bind(R.id.email_field)EditText mEmail;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        ButterKnife.bind(this);
+
+    }
+
+    public void signUp(View view){
+
+        String userName = mUserName.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        String email = mEmail.getText().toString().trim();
+
+        if (userName.isEmpty() || password.isEmpty() || email.isEmpty()){
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.sign_up_error_message);
+            builder.setTitle(R.string.sign_error_title);
+            builder.setPositiveButton(android.R.string.ok, null);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+        }
+        else{
+            //create the new user
+            ParseUser newUser = new ParseUser();
+            newUser.setUsername(userName);
+            newUser.setPassword(password);
+            newUser.setEmail(email);
+
+            newUser.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e == null){
+                        //Succesfull
+
+                        Intent intent = new  Intent(SignUpActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+                    else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                        builder.setMessage(e.getMessage());
+                        builder.setTitle(R.string.sign_error_title);
+                        builder.setPositiveButton(android.R.string.ok, null);
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sign_up, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
