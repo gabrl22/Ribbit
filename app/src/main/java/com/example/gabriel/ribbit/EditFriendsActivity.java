1510dbcacdb48d1c1d4a1eb187e18a545d7f9c21
Gabriel Cole <lgcw92@gmail.com>
2015-08-31 06:09:59 -0500

    added listview in appcompatactivity

--- app/src/main/java/com/example/gabriel/ribbit/EditFriendsActivity.java
+++ app/src/main/java/com/example/gabriel/ribbit/EditFriendsActivity.java
@@ -1,19 +1,23 @@
 package com.example.gabriel.ribbit;
 
 import android.app.AlertDialog;
-import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -21,17 +25,25 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 
-public class EditFriendsActivity extends ListActivity {
+public class EditFriendsActivity extends AppCompatActivity {
 
     protected static final String TAG = EditFriendsActivity.class.getSimpleName();
     protected List<ParseUser> mParseUsers;
+    protected ParseRelation<ParseUser> mFriendsRelation;
+    protected ParseUser mCurrentUser;
     @Bind(R.id.progressBar)ProgressBar mProgressBar;
+    @Bind(R.id.listview)ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_friends);
         ButterKnife.bind(this);
+        mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+
+
+
 
 
     }
@@ -39,7 +51,10 @@ public class EditFriendsActivity extends ListActivity {
     protected void onResume()
     {
         super.onResume();
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
         mProgressBar.setVisibility(View.VISIBLE);
+        mListView.setVisibility(View.INVISIBLE);
 
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.orderByAscending(ParseConstants.KEY_USERNAME);
@@ -50,6 +65,7 @@ public class EditFriendsActivity extends ListActivity {
             public void done(List<ParseUser> users, ParseException e) {
 
                 mProgressBar.setVisibility(View.VISIBLE);
+                mListView.setVisibility(View.VISIBLE);
 
                 if (e == null){
                     //Success
@@ -63,10 +79,27 @@ public class EditFriendsActivity extends ListActivity {
                     }
                     ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                             EditFriendsActivity.this,
-                            android.R.layout.simple_list_item_1,
+                            android.R.layout.simple_list_item_checked,
                             usernames
                     );
-                    setListAdapter(adapter);
+                    mListView.setAdapter(adapter);
+                    mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            mFriendsRelation.add(mParseUsers.get(position));
+                            mCurrentUser.saveInBackground(new SaveCallback() {
+                                @Override
+                                public void done(ParseException e) {
+                                    //
+                                    if(e != null){
+                                        Log.e(TAG, e.getMessage());
+                                    }
+                                }
+                            });
+
+                        }
+                    });
                 }
                 else {
                     mProgressBar.setVisibility(View.INVISIBLE);
@@ -103,4 +136,5 @@ public class EditFriendsActivity extends ListActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
